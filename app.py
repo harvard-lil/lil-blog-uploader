0f6de22cfcd39604980fe960d4a84c927ffda7fa
annabialas <ab5086@nyu.edu>
2017-07-11 16:07:31 -0400

    makes it compatible with my s3 bucket

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ from proxy import proxy_request
 import logging
 
 app = Flask(__name__)
-app.config['GITHUB_CLIENT_ID'] = environ.get('GITHUB_CLIENT_ID')
-app.config['GITHUB_CLIENT_SECRET'] = environ.get('GITHUB_CLIENT_SECRET')
-app.config['GITHUB_ORG_NAME'] = environ.get('GITHUB_ORG_NAME')
+# app.config['GITHUB_CLIENT_ID'] = environ.get('GITHUB_CLIENT_ID')
+# app.config['GITHUB_CLIENT_SECRET'] = environ.get('GITHUB_CLIENT_SECRET')
+# app.config['GITHUB_ORG_NAME'] = environ.get('GITHUB_ORG_NAME')
 app.config['SECRET_KEY'] = environ.get('FLASK_SECRET_KEY')
-app.config['SESSION_COOKIE_SECURE'] = literal_eval(environ.get('SESSION_COOKIE_SECURE', 'True'))
-app.config['LOGIN_EXPIRY_MINUTES'] = environ.get('LOGIN_EXPIRY', 30)
-app.config['LOG_LEVEL'] = environ.get('LOG_LEVEL', 'WARNING')
+# app.config['SESSION_COOKIE_SECURE'] = literal_eval(environ.get('SESSION_COOKIE_SECURE', 'True'))
+# app.config['LOGIN_EXPIRY_MINUTES'] = environ.get('LOGIN_EXPIRY', 30)
+# app.config['LOG_LEVEL'] = environ.get('LOG_LEVEL', 'WARNING')
 # Specific to this proxy
 app.config['MAX_CONTENT_LENGTH'] = environ.get('MAX_CONTENT_LENGTH', 16 * 1024 * 1024) ## 16MB
 app.config['S3_BUCKET'] = environ.get('S3_BUCKET')
@@ -28,50 +28,50 @@ app.config['AWS_SECRET_ACCESS_KEY'] = environ.get('AWS_SECRET_ACCESS_KEY')
 # register error handlers
 error_handling.init_app(app)
 
-AUTHORIZE_URL = 'https://github.com/login/oauth/authorize'
-ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token'
-ORGS_URL = 'https://api.github.com/user/orgs'
-REVOKE_TOKEN_URL = 'https://api.github.com/applications/{}/tokens/'.format(app.config['GITHUB_CLIENT_ID'])
+# AUTHORIZE_URL = 'https://github.com/login/oauth/authorize'
+# ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token'
+# ORGS_URL = 'https://api.github.com/user/orgs'
+# REVOKE_TOKEN_URL = 'https://api.github.com/applications/{}/tokens/'.format(app.config['GITHUB_CLIENT_ID'])
 
 
 ###
 ### UTILS ###
 ###
 
-@app.before_first_request
-def setup_logging():
-    if not app.debug:
-        # In production mode, add log handler to sys.stderr.
-        app.logger.addHandler(logging.StreamHandler())
-        app.logger.setLevel(getattr(logging, app.config['LOG_LEVEL']))
+# @app.before_first_request
+# def setup_logging():
+#     if not app.debug:
+#         # In production mode, add log handler to sys.stderr.
+#         app.logger.addHandler(logging.StreamHandler())
+#         app.logger.setLevel(getattr(logging, app.config['LOG_LEVEL']))
 
-def login_required(func):
-    @wraps(func)
-    def handle_login(*args, **kwargs):
-        logged_in = session.get('logged_in')
-        valid_until = session.get('valid_until')
-        if valid_until:
-            valid = datetime.strptime(valid_until, '%Y-%m-%d %H:%M:%S') > datetime.utcnow()
-        else:
-            valid = False
-        if logged_in and logged_in == "yes" and valid:
-            app.logger.debug("User session valid")
-            return func(*args, **kwargs)
-        else:
-            app.logger.debug("Redirecting to GitHub")
-            session['next'] = request.url
-            return redirect('{}?scope=user&client_id={}'.format(AUTHORIZE_URL, app.config['GITHUB_CLIENT_ID']))
-    return handle_login
+# def login_required(func):
+#     @wraps(func)
+#     def handle_login(*args, **kwargs):
+#         logged_in = session.get('logged_in')
+#         valid_until = session.get('valid_until')
+#         if valid_until:
+#             valid = datetime.strptime(valid_until, '%Y-%m-%d %H:%M:%S') > datetime.utcnow()
+#         else:
+#             valid = False
+#         if logged_in and logged_in == "yes" and valid:
+#             app.logger.debug("User session valid")
+#             return func(*args, **kwargs)
+#         else:
+#             app.logger.debug("Redirecting to GitHub")
+#             session['next'] = request.url
+#             return redirect('{}?scope=user&client_id={}'.format(AUTHORIZE_URL, app.config['GITHUB_CLIENT_ID']))
+#     return handle_login
 
-def is_safe_url(target):
-    '''
-        Ensure a url is safe to redirect to, from WTForms
-        http://flask.pocoo.org/snippets/63/from WTForms
-    '''
-    ref_url = urlparse(request.host_url)
-    test_url = urlparse(urljoin(request.host_url, target))
-    return test_url.scheme in ('http', 'https') and \
-           ref_url.netloc == test_url.netloc
+# def is_safe_url(target):
+#     '''
+#         Ensure a url is safe to redirect to, from WTForms
+#         http://flask.pocoo.org/snippets/63/from WTForms
+#     '''
+#     ref_url = urlparse(request.host_url)
+#     test_url = urlparse(urljoin(request.host_url, target))
+#     return test_url.scheme in ('http', 'https') and \
+#            ref_url.netloc == test_url.netloc
 
 ###
 ### ROUTES
@@ -79,7 +79,7 @@ def is_safe_url(target):
 
 @app.route('/', defaults={'path': ''}, methods=['GET', 'POST'])
 @app.route('/<path:path>')
-@login_required
+# @login_required
 def catch_all(path):
     '''
         All requests are caught by this route, unless explicitly caught by
@@ -102,58 +102,59 @@ def catch_all(path):
         return fallback()
 
 
-@app.route('/login')
-def login():
-    return render_template('login.html', context={'heading': 'Log In', 'client_id': app.config['GITHUB_CLIENT_ID']})
 
-@app.route("/logout")
-def logout():
-    session.clear()
-    return render_template('generic.html', context={'heading': "Logged Out",
-                                                    'message': "You have successfully been logged out."})
+# @app.route('/login')
+# def login():
+#     return render_template('login.html', context={'heading': 'Log In', 'client_id': app.config['GITHUB_CLIENT_ID']})
 
-@app.route('/auth/github/callback')
-def authorized():
-    app.logger.debug("Requesting Access Token")
-    r = requests.post(ACCESS_TOKEN_URL, headers={'accept': 'application/json'},
-                                        data={'client_id': app.config['GITHUB_CLIENT_ID'],
-                                              'client_secret': app.config['GITHUB_CLIENT_SECRET'],
-                                              'code': request.args.get('code')})
-    data = r.json()
-    if r.status_code == 200:
-        access_token = data.get('access_token')
-        scope = data.get('scope')
-        app.logger.debug("Received Access Token")
-    else:
-        app.logger.error("Failed request for access token. Gitub says {}".format(data['message']))
-        abort(500)
+# @app.route("/logout")
+# def logout():
+#     session.clear()
+#     return render_template('generic.html', context={'heading': "Logged Out",
+#                                                     'message': "You have successfully been logged out."})
 
-    if scope == 'user':
-        app.logger.debug("Requesting User Organization Info")
-        r = requests.get(ORGS_URL, headers={'accept': 'application/json',
-                                            'authorization': 'token {}'.format(access_token)})
+# @app.route('/auth/github/callback')
+# def authorized():
+#     app.logger.debug("Requesting Access Token")
+#     r = requests.post(ACCESS_TOKEN_URL, headers={'accept': 'application/json'},
+#                                         data={'client_id': app.config['GITHUB_CLIENT_ID'],
+#                                               'client_secret': app.config['GITHUB_CLIENT_SECRET'],
+#                                               'code': request.args.get('code')})
+#     data = r.json()
+#     if r.status_code == 200:
+#         access_token = data.get('access_token')
+#         scope = data.get('scope')
+#         app.logger.debug("Received Access Token")
+#     else:
+#         app.logger.error("Failed request for access token. Gitub says {}".format(data['message']))
+#         abort(500)
 
-        app.logger.debug("Revoking Github Access Token")
-        d = requests.delete(REVOKE_TOKEN_URL + access_token, auth=(app.config['GITHUB_CLIENT_ID'], app.config['GITHUB_CLIENT_SECRET']))
-        app.logger.debug("(Request returned {})".format(d.status_code))
+#     if scope == 'user':
+#         app.logger.debug("Requesting User Organization Info")
+#         r = requests.get(ORGS_URL, headers={'accept': 'application/json',
+#                                             'authorization': 'token {}'.format(access_token)})
 
-        data = r.json()
-        if r.status_code == 200:
-            if data and any(org['login'] == app.config['GITHUB_ORG_NAME'] for org in data):
-                next = session.get('next')
-                session.clear()
-                valid_until = (datetime.utcnow() + timedelta(seconds=60*30)).strftime('%Y-%m-%d %H:%M:%S')
-                session['valid_until'] = valid_until
-                session['logged_in'] = "yes"
-                if next and is_safe_url(next):
-                    return redirect(next)
-                return redirect(url_for('catch_all'))
-            else:
-                app.logger.warning("Log in attempt from Github user who is not a member of LIL.")
-                abort(401)
-        else:
-            app.logger.error("Failed request for user orgs. Gitub says {}".format(data['message']))
-            abort(500)
-    else:
-        app.logger.warning("Insufficient scope authorized in Github; verify API hasn't changed.")
-        abort(401)
+#         app.logger.debug("Revoking Github Access Token")
+#         d = requests.delete(REVOKE_TOKEN_URL + access_token, auth=(app.config['GITHUB_CLIENT_ID'], app.config['GITHUB_CLIENT_SECRET']))
+#         app.logger.debug("(Request returned {})".format(d.status_code))
+
+#         data = r.json()
+#         if r.status_code == 200:
+#             if data and any(org['login'] == app.config['GITHUB_ORG_NAME'] for org in data):
+#                 next = session.get('next')
+#                 session.clear()
+#                 valid_until = (datetime.utcnow() + timedelta(seconds=60*30)).strftime('%Y-%m-%d %H:%M:%S')
+#                 session['valid_until'] = valid_until
+#                 session['logged_in'] = "yes"
+#                 if next and is_safe_url(next):
+#                     return redirect(next)
+#                 return redirect(url_for('catch_all'))
+#             else:
+#                 app.logger.warning("Log in attempt from Github user who is not a member of LIL.")
+#                 abort(401)
+#         else:
+#             app.logger.error("Failed request for user orgs. Gitub says {}".format(data['message']))
+#             abort(500)
+#     else:
+#         app.logger.warning("Insufficient scope authorized in Github; verify API hasn't changed.")
+#         abort(401)
--- proxy.py
+++ proxy.py
@@ -122,8 +122,8 @@ def proxy_request(request, path):
             aws_access_key_id=current_app.config['AWS_ACCESS_KEY_ID'],
             aws_secret_access_key=current_app.config['AWS_SECRET_ACCESS_KEY'],
         )
-        s3.upload_fileobj(f.stream, 'lil-blog-media', filename, ExtraArgs={'ContentType': get_mime_type(filename)})
+        s3.upload_fileobj(f.stream, 'archive-my-trash', filename, ExtraArgs={'ContentType': get_mime_type(filename)})
         return render_template('success.html', context={'heading': "Your file is up!" ,
                                                         'url': "https://{}.s3.amazonaws.com/{}".format(current_app.config['S3_BUCKET'], filename) })
-    return render_template('uploader.html', context={'heading': 'Upload Media', 'limit': current_app.config['MAX_CONTENT_LENGTH']/1024/1024}, form=form)
+    return render_template('uploader.html', context={'heading': 'Upload Media', 'limit': current_app.config['MAX_CONTENT_LENGTH']}, form=form)
 
